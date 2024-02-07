94700634d982478e67777760ec0d849c9d937c74
Salih Yasin Seker <salihyasinseker@gmail.com>
2024-02-07 14:59:56 +0300

    Login page designs.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
-import React, { useState, useRef } from 'react';
-import { useAuth } from '@/app/contexts/auth.context';
+import React, { useState, useRef } from "react";
+import { useAuth } from "@/app/contexts/auth.context";
+import Button from "@/app/components/button";
 
 export default function Login() {
   const form = useRef();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [formMessage, setFormMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [formMessage, setFormMessage] = useState("");
   const { login } = useAuth();
 
   const handleLogin = async (e) => {
@@ -18,60 +19,56 @@ export default function Login() {
     setFormMessage(loginReq);
   };
   return (
-    <div className=" min-h-screen bg-primary-black flex items-center">
+    <div className="login flex items-center min-h-[85vh]">
       <form
         onSubmit={handleLogin}
         ref={form}
-        className=" w-full max-w-2xl mx-auto"
+        className="bg-primary-black w-full max-w-2xl mx-auto flex flex-col items-center gap-8 py-14 px-6 md:py-28 md:px-40 ml-4 mr-4 md:m-auto"
       >
-        <div className="mb-5">
-          <label
+        {/* <label
             htmlFor="username"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="username"
-            required
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-        </div>
-        <div className="mb-5">
-          <label
+          </label> */}
+        <input
+          type="text"
+          id="username"
+          className="text-white bg-transparent border-b border-[#363E5B] rounded-none text-sm  block w-full max-w-80 p-2.5 text-center"
+          placeholder="username"
+          required
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+
+        {/* <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
+          </label> */}
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="text-white bg-transparent border-b border-[#363E5B] rounded-none text-sm  block w-full max-w-80 p-2.5 text-center"
+          required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+
         {formMessage && (
           <div className="p-4 text-sm text-blue-800 rounded-lg bg-blue-50 ">
             {formMessage}
           </div>
         )}
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
+        <Button type="submit" className={`w-full max-w-80`}>
+          Login
+        </Button>
       </form>
     </div>
   );
